6c81adf9370722f9e39cc90e1df855a488d9431b
B533P-12\533 <pellthomas@hotmail.com>
2018-09-14 14:48:27 -0400

    Cours7

---
+++ Cours4/Assets/Script/DialogManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour {
+    [SerializeField] public GameObject dialogPrefab;
+    [SerializeField] public GameObject mainCanvas;
+
+
+    private bool actionAxisInUse = true;
+    private GameObject player;
+    private bool dialogIsInitiated = false;
+    private DialogText currentDialog;
+    private DialogDisplayer currentDialogDisplayer;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    public void Startdialog(DialogText newDialog)
+    {
+        dialogIsInitiated = true;
+        player.GetComponent<PlayerMovement>().DisableControl();
+        currentDialog = newDialog;
+        GameObject currentDialogObject = Instantiate(dialogPrefab, mainCanvas.transform);
+        currentDialogDisplayer = currentDialogObject.GetComponent<DialogDisplayer>();
+        currentDialogDisplayer.SetDialogText(currentDialog.GetDialogText());
+    }
+
+    public void ProcessInput()
+    {
+        if (ShouldProcessInput())
+        {
+            actionAxisInUse = true;
+            if (currentDialog.IsNextDialog())
+            {
+                currentDialog = currentDialog.GetNextDialog();
+                currentDialogDisplayer.SetDialogText(currentDialog.GetDialogText());
+            }
+            else
+            {
+                EndDialog();
+            }
+
+        }
+    }
+
+    public void EndDialog()
+    {
+        dialogIsInitiated = false;
+        currentDialogDisplayer.CloseDialog();
+        player.GetComponent<PlayerMovement>().EnableControl();
+        currentDialog = null;
+    }
+
+    private bool ShouldProcessInput()
+    {
+        if (dialogIsInitiated)
+        {
+            if (!actionAxisInUse && Input.GetAxis("Jump") != 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ValideAxisInUse()
+    {
+        if (Input.GetAxis("Jumo") != 0)
+        {
+            actionAxisInUse = true;
+        }
+        else
+        {
+            actionAxisInUse = false;
+        }
+
+
+    }
+
+
+
+
+}
--- Cours4/Assets/Script/InteractionTalk.cs
+++ Cours4/Assets/Script/InteractionTalk.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractionTalk : Interaction {
+    DialogManager dialogManager;
+    [SerializeField] DialogText dialogText;
+
     public override void Interact()
     {
-        print("Hello");
+        dialogManager.Startdialog(dialogText);
     }
 
     // Use this for initialization
     void Start () {
-		
+        dialogManager = GameObject.FindGameObjectWithTag("DialogManager").GetComponent<DialogManager>();
 	}
 	
 	// Update is called once per frame
